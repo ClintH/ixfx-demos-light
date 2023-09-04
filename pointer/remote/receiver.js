72e91c342f506b59745549e0455045f1023eb8be
CH <clint@thestaticvoid.net>
2023-09-04 17:44:10 +0200

    Updated websocket readme

--- pointer/remote/receiver.js
+++ pointer/remote/receiver.js
@@ -1,5 +1,7 @@
 // @ts-ignore
 import { Remote } from "https://unpkg.com/@clinth/remote@latest/dist/index.mjs";
+// import { inlineConsole } from "https://unpkg.com/ixfx/dist/dom.js";
+// inlineConsole();
 
 const settings = Object.freeze({
   remote: new Remote({
